f45e5f17acae5798a7969ce23bf1be109e6d268b
RuanVR <101905932+RuanVR@users.noreply.github.com>
2022-12-16 12:49:43 -0300

    ADD JS

---
+++ JS/aula16-12/script.js
@@ -0,0 +1,16 @@
+function executar(){
+    alert('mensagem de alerta')
+}
+
+function entrar(){
+    document.bgColor = "red"
+}
+
+function sair(){
+    document.bgColor = "white"
+}
+
+function abrir(){
+    //window.location.replace('http//www.ufes.br')
+    
+}
